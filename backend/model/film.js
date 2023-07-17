dfd5e6ac727d1d2e4c16f665a1dc5ad1bbd973c5
keithtang6969 <keifotango@gmail.com>
2023-07-17 22:21:37 +0800

    final

--- backend/model/film.js
+++ backend/model/film.js
@@ -1,12 +1,3 @@
-/*
-Name: Tang Zheng Yin Keith
-Admission Number: 2228002
-Class: DIT/FT/1B/03
-ST0503 BACK-END WEB DEVELOPMENT
-2022/2023 SEMESTER 2
-ASSIGNMENT 2 SOURCE CODE
-*/
-
 var db = require('./databaseConfig.js');
 
 var film = {
